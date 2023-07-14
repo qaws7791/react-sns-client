9aa8b408cdb066bbb2853ff4c80482f8ee59baf0
qaws7791 <qaws7791@gmail.com>
2023-07-14 11:18:44 +0900

    fix: config 수정

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,5 @@ import jsconfigPaths from "vite-jsconfig-paths";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), jsconfigPaths()],
-    esbuild: {
-      pure: mode === "production" ? ["console.log"] : [],
-    },
   };
 });
